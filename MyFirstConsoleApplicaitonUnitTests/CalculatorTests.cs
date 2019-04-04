c491965d66867c2b807313d8f9c98a62fb85c871
Brent McManus <bmcmanus@pdx.edu>
2019-04-03 19:47:01 -0700

    Added test class with tests

---
+++ MyFirstConsoleApplicaitonUnitTests/CalculatorTests.cs
@@ -0,0 +1,40 @@
+﻿using FluentAssertions;
+using MyFirstConsoleApplication;
+using System;
+using Xunit;
+
+namespace MyFirstConsoleApplicaiton
+{
+    public class CalculatorTests
+    {
+        [Fact]
+        public void Calculator_Add_Success()
+        {
+            var calc = new Calculator();
+            var first = 12;
+            var second = 12;
+            var result = calc.add(first, second);
+            result.Should().Be(24, "Because addition...");
+        }
+
+        [Fact]
+        public void Calculator_Add_FirstZeroThrows()
+        {
+            var calc = new Calculator();
+
+            Action action = () => calc.add(0, 12);
+
+            action.Should().Throw<ArgumentOutOfRangeException>().Where(e => e.ParamName == "first");
+        }
+
+        [Fact]
+        public void Calculator_Add_SecondZeroThrows()
+        {
+            var calc = new Calculator();
+
+            Action action = () => calc.add(12, 0);
+
+            action.Should().Throw<ArgumentOutOfRangeException>().Where(e => e.ParamName == "second");
+        }
+    }
+}
---
+++ MyFirstConsoleApplication/Calculator.cs
@@ -0,0 +1,28 @@
+﻿using EnsureThat;
+using System;
+
+namespace MyFirstConsoleApplication
+{
+    public class Calculator
+    {
+        int total;
+
+        public int add(int first, int second)
+        {
+            Ensure.That(first, nameof(first)).IsGt(0);
+            Ensure.That(second, nameof(second)).IsGt(0);
+            return first + second;
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var calculator = new Calculator();
+
+            Console.WriteLine(calculator.add(6, 6));
+
+        }
+    }
+}
